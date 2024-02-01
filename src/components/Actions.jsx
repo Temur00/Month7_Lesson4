d76223c38357bd9c574149f104bc6ade26d518c7
Temur <shernazarovtemurcom001@gmail.com>
2024-02-01 14:19:57 +0500

    table has been added

--- src/components/Actions.jsx
+++ src/components/Actions.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import IconButton from "@mui/material/IconButton";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { Stack } from "@mui/material";
 
 const Actions = () => {
-  return <div>Actions</div>;
+  return (
+    <Stack direction="row" spacing={1}>
+      <div
+        style={{
+          width: "38",
+          height: "38",
+          background: "#efefef",
+          "border-radius": "12",
+        }}
+      >
+        <IconButton color="success">
+          <EditIcon />
+        </IconButton>
+      </div>
+      <div
+        style={{
+          width: "40",
+          height: "40",
+          background: "#efefef",
+          "border-radius": "12",
+        }}
+      >
+        <IconButton color="error">
+          <DeleteIcon />
+        </IconButton>
+      </div>
+    </Stack>
+  );
 };
 
 export default Actions;
