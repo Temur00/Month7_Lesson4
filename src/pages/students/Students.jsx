b6cdb056f83cb10814244b1b95e327f81146da12
Temur <shernazarovtemurcom001@gmail.com>
2024-02-01 16:24:10 +0500

    students also added from mock api

--- src/pages/students/Students.jsx
+++ src/pages/students/Students.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Paper,
   Table,
@@ -10,78 +10,15 @@ import {
 } from "@mui/material";
 import Actions from "../../components/Actions";
 
-const columns = [
-  { field: "id", headerName: "ID", width: 70 },
-  { field: "firstName", headerName: "First name", width: 130 },
-  { field: "lastName", headerName: "Last name", width: 130 },
-  {
-    field: "age",
-    headerName: "Age",
-    type: "number",
-    width: 90,
-  },
-  {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
-  },
-];
-
-const rows = [
-  { id: 1, lastName: "Wilson", firstName: "Robert", age: 35, level: "middle" },
-  {
-    id: 2,
-    lastName: "Lannister",
-    firstName: "Cersei",
-    age: 42,
-    level: "junior",
-  },
-  {
-    id: 3,
-    lastName: "Lannister",
-    firstName: "Jaime",
-    age: 45,
-    level: "junior",
-  },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16, level: "senior" },
-  {
-    id: 5,
-    lastName: "Targaryen",
-    firstName: "Daenerys",
-    age: 25,
-    level: "middle",
-  },
-  {
-    id: 6,
-    lastName: "Melisandre",
-    firstName: "Tom",
-    age: 150,
-    level: "junior",
-  },
-  {
-    id: 7,
-    lastName: "Clifford",
-    firstName: "Ferrara",
-    age: 44,
-    level: "senior",
-  },
-  {
-    id: 8,
-    lastName: "Frances",
-    firstName: "Rossini",
-    age: 36,
-    level: "junior",
-  },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65, level: "middle" },
-];
-
-const mappedRows = rows.map((row) => ({ ...row, actions: <Actions /> }));
-
 const Students = () => {
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    fetch("https://65bb677f52189914b5bc02b7.mockapi.io/students")
+      .then((response) => response.json())
+      .then((data) => setStudents(data))
+      .catch((error) => console.error("Error fetching students:", error));
+  }, []);
   return (
     <div>
       <h2>Students</h2>
@@ -98,21 +35,14 @@ const Students = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {mappedRows.map((row) => (
-              <TableRow
-                key={row.id}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {row.id}
-                </TableCell>
-                <TableCell>{row.firstName}</TableCell>
-                <TableCell>{row.lastName}</TableCell>
-                <TableCell>{row.age}</TableCell>
-                <TableCell>{row.level}</TableCell>
-                <TableCell align="center">
-                  <Actions align="center" />
-                </TableCell>
+            {students.map((student) => (
+              <TableRow key={student.id}>
+                <TableCell>{student.id}</TableCell>
+                <TableCell>{student.firstName}</TableCell>
+                <TableCell>{student.lastName}</TableCell>
+                <TableCell>{student.age}</TableCell>
+                <TableCell>{student.level}</TableCell>
+                <Actions align="center" />
               </TableRow>
             ))}
           </TableBody>
--- src/server/main.js
+++ src/server/main.js
@@ -1,7 +1,16 @@
 import axios from "axios";
 
 const request = axios.create({
+  timeout: 10000,
+});
+
+export const teachersRequest = axios.create({
   baseURL: "https://65bb677f52189914b5bc02b7.mockapi.io/teachers",
+  timeout: 10000,
+});
+
+export const studentsRequest = axios.create({
+  baseURL: "https://65bb677f52189914b5bc02b7.mockapi.io/students",
   timeout: 10000,
 });
 
